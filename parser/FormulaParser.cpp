09d8f4bf8f959770427a6b2a172eb3d4e6d624b1
drrkkm <vikei.fafg@yandex.ru>
2023-05-08 22:41:05 +0300

    major update of parser

--- parser/FormulaParser.cpp
+++ parser/FormulaParser.cpp
@@ -1,5 +1,10 @@
 #include "FormulaParser.hpp"
+#include "operands.hpp"
 
+typedef boost::tokenizer<boost::escaped_list_separator<char>> Tokenizer;
+equality_op_t::equality_op_t(){
+    add ("=", EqualityExpression::Equality);
+}
 
 unary_op_t::unary_op_t() {
     add("+",   UnaryExpression::Plus);
@@ -17,72 +22,166 @@ binary_op::binary_op(int precedence) {
             add("*",   BinaryExpression::Mul);
             add("/",   BinaryExpression::Div);
             add("mod", BinaryExpression::Mod);
-            return;std::to_string(precedence);
+            return;
         case 3:
             add("**", BinaryExpression::Pow);
             return;
-        }
         throw std::runtime_error("Unknown precedence ");
+    }
 }
 
+void FormulaParser::parse(std::string input_t){
 
-Expression FormulaParser::parse(const char* input) {
-    auto end = input + std::strlen(input);
-    Expression result;
-    auto ok = phrase_parse(input, end, expr, x3::space, result);
-    if (ok && input == end) return result;
-    throw std::runtime_error(std::string("Failed at: `") + input + "`");
+    input_t.erase(std::remove_if(input_t.begin(), input_t.end(), isspace), input_t.end());
+
+    if(input_t.find("=") == 0) throw std::runtime_error("Тo data to the right of equality");
+    else if(input_t.find("=") >= input_t.size()) throw std::runtime_error("No equality");
+    else if(input_t.find("=") == 0) throw std::runtime_error("Тo data to the right of equality");
+    else if(input_t.find("=") == input_t.rfind("=")){
+        std::string first_token_t = input_t.substr(0, input_t.find("="));
+        std::string second_token_t = input_t.substr(input_t.find("=")+1, input_t.size() - 1);
+
+        const char * first_token = first_token_t.c_str();
+        const char * second_token = second_token_t.c_str();
+
+        auto f_end = first_token + std::strlen(first_token);
+        auto s_end = second_token + std::strlen(second_token);
+
+        Expression f_result;
+        Expression s_result;
+        auto f_ok = phrase_parse(first_token, f_end, expr, x3::space, f_result);
+        auto s_ok = phrase_parse(second_token, s_end, expr, x3::space, s_result);
+
+        Node firstNode = Node(f_result);
+        Node secondNode = Node(s_result);
+
+        if (f_ok && first_token == f_end && s_ok && second_token == s_end) Equality(f_result, s_result);
+        //throw std::runtime_error(std::string("Failed at: `") + first + "`");
+        std::cout << "FormulaParser::parse(exp) : " << exp << "\n";
+    }
+    else throw std::runtime_error("To much equality");
 }
 
-double FormulaParser::eval_binary(BinaryExpression::op_t op, double a, double b) { 
+void FormulaParser::Equality(Expression first, Expression second) { 
+
+    auto visitor = boost::make_overloaded_function(
+        [](double x) -> std::vector<double> { throw std::runtime_error("You cannot assign anything to a number"); },
+
+        [&](const UnaryExpression& first) -> std::vector <double> {
+            throw std::runtime_error("You cannot assign anything to a unary operation");
+        },
+        [&](const FunctionCall& first) -> std::vector<double> {
+           //save function
+        },
+        [&](const BinaryExpression& first) -> std::vector<double> {
+            throw std::runtime_error("You cannot assign anything to a binary operation");
+        }  
+        ,
+        [&](const VariableExpression &first) -> std::vector<double> {
+            auto data = eval(second);
+            if (data.size() > 1) throw std::runtime_error("You can't assign multiple values to a constant");
+            constants[first.name] = data[0];
+        },
+        [&](const IndexExpression &first) -> std::vector<double> {
+            auto data = eval(second);
+            std::vector<double> &manager_data = manager.getRowData(first.name);
+            if (first.args.size() == 1 && data.size() == 1){
+                manager_data[first.args[0]] = data[0];
+            }  // manager.return_variable(e.name, e.args.at(0));
+            if (first.args.size() == 2 && first.args[0] - first.args[1] == data.size()) {
+                for (int item = first.args[0]; item <= first.args[1]; item++)
+                    manager_data[first.args[item]] = data[item];
+            }
+            throw std::runtime_error("Too much indexes");
+        });
+    return boost::apply_visitor(visitor, first, second);
+}
+
+std::vector<double> FormulaParser::eval_binary(BinaryExpression::op_t op, std::vector<double> a, std::vector<double> b) { 
+
     switch (op) {
     case BinaryExpression::Plus: return a + b;
     case BinaryExpression::Minus: return a - b;
     case BinaryExpression::Mul: return a * b;
     case BinaryExpression::Div: return a / b;
-    case BinaryExpression::Mod: return (int)a % (int)b;
-    case BinaryExpression::Pow: return pow(a, b);
+    case BinaryExpression::Pow: return a ^ b;
     default: throw std::runtime_error("Unknown operator");
     }
 }
-
-double FormulaParser::eval(Expression e) {
+//std::vector<double> errors
+std::vector<double> FormulaParser::eval(Expression e) {
+    std::vector <double> data{};
     auto visitor = boost::make_overloaded_function(
-        [](double x) { return x; },
-        [&](const UnaryExpression& e) -> double {
-            auto a = eval(e.arg);
+        [](double x) -> std::vector<double> {
+            return std::vector<double>{x}; 
+        },
+        [&](const UnaryExpression& e) -> std::vector <double> {
+            auto a = eval(e.arg)[0];
             switch (e.op) {
-            case UnaryExpression::Plus: return +a;
-            case UnaryExpression::Minus: return -a;
+                case UnaryExpression::Plus: return std::vector<double>{+a};
+                case UnaryExpression::Minus: return std::vector<double>{-a};
             }
             throw std::runtime_error("Unknown operator");
         },
-        [&](const FunctionCall& e) -> double {
-            
-            std::vector <double> args;
-            for (int item = 0; item < e.args.size(); item++){
-                args.push_back(eval(e.args.at(item)));
+        [&](const FunctionCall& e) -> std::vector<double> {
+            std::vector <double> args = eval(e.args[0]);
+            for (int item = 1; item < e.args.size(); item++){
+                auto c = eval(e.args[item]);
+                args.insert(args.end(), c.begin(), c.end());
             }
-            //auto args = [&](int i) { return eval(e.args.at(i)); };
-            boost::filesystem::path lib_path(".");          // argv[1] contains path to directory with our plugin library
-            boost::shared_ptr<plugin_api> plugin = nullptr; 
-            std::string plugin_name = "my_plugin_" + e.function;
-            plugin = dll::import_symbol<plugin_api>(lib_path / plugin_name, "plugin", dll::load_mode::append_decorations);
-            return plugin->calculate(args);
-            throw std::runtime_error("Unknown function");
+            try{
+                boost::filesystem::path lib_path(".");
+                boost::shared_ptr<plugin_api> plugin = nullptr; 
+                std::string plugin_name = "plugin_" + e.function;
+                plugin = dll::import_symbol<plugin_api>(lib_path / plugin_name, "plugin", dll::load_mode::append_decorations);
+                return std::vector<double>{plugin->calculate(args)};
+            }
+            catch (...){
+                throw std::runtime_error("Unknown function");
+            }
         },
-        [&](const BinaryExpression& e) -> double {   
+        [&](const VariableExpression &e) -> std::vector<double> {
+            auto a = e.name;
+            if (constants.find(a) != constants.end()) return std::vector<double>{constants[a]};
+            throw std::runtime_error("Unknown variable");
+        },
+        [&](const IndexExpression &e) -> std::vector<double> {
+            std::vector<double> &manager_data = manager.getRowData(e.name);
+
+            if (e.args.size() == 1 && manager_data.size() >= 1) return std::vector<double>{manager_data[e.args[0]]}; // manager.return_variable(e.name, e.args.at(0));
+            if (e.args.size() == 2 && manager_data.size() >= 2) {
+                if (e.args[0] - e.args[1] < 0) throw std::runtime_error("Wrong indexes");
+                for (int item = e.args[0]; item <= e.args[1]; item++) {
+                        data.push_back(manager_data[item]);
+                    }
+                    return data;
+            }
+            throw std::runtime_error("Too much indexes");
+        },
+        [&](const BinaryExpression& e) -> std::vector<double> {   
             auto a = eval(e.first);
             for (auto&& o : e.ops) {
                 auto b = eval(o.second);
                 a = eval_binary(o.first, a, b);
             }
             return a;
-        },
-        [](const VariableExpression &e) -> double {
-            auto a = e.name;
-            std::cout << a;
-            return 8;
         });
     return boost::apply_visitor(visitor, e);
+}
+
+void FormulaParser::load_constants(std::string path){
+    std::string name;
+    double value = 0;
+    std::fstream fin;
+    try{
+        fin.open(path + "constants.csv", std::ios::in);
+        while(fin){
+            fin >> name >> value;
+            constants[name] = value;
+        }
+        fin.close();
+    }
+    catch (...){
+        throw std::runtime_error("No constant's file");
+    }
 }
--- parser/FormulaParser.hpp
+++ parser/FormulaParser.hpp
@@ -1,6 +1,11 @@
 #include <string>
 #include <vector>
+#include <map>
 #include <iostream>
+#include <fstream>
+
+#include <boost/tokenizer.hpp>
+#include <boost/algorithm/algorithm.hpp>
 
 #include <boost/fusion/adapted.hpp>
 #include <boost/variant.hpp>
@@ -9,19 +14,31 @@
 #include <boost/spirit/home/x3.hpp>
 
 #include "plugin_api.hpp"
+#include "utils.hpp"
+
+//#include "../manager.h"
 
 namespace dll = boost::dll;
 namespace x3 = boost::spirit::x3;
 
+
 using Expression = boost::variant<
-      double
+    double
     , boost::recursive_wrapper<struct VariableExpression>
     , boost::recursive_wrapper<struct UnaryExpression>
-    , boost::recursive_wrapper<struct BinaryExpression>
     , boost::recursive_wrapper<struct FunctionCall>
-    // , boost::recursive_wrapper<struct IndexExpression>
+    , boost::recursive_wrapper<struct IndexExpression>
+    , boost::recursive_wrapper<struct VariableExpression>
+    , boost::recursive_wrapper<struct BinaryExpression>
     >;
 
+struct Node{
+    Expression expression;
+    std::vector<double> errors{};
+
+    Node(Expression e) : expression(e){};
+    Node(Expression e, std::vector<double> errors) : expression(e), errors(errors){};
+};
 
 struct VariableExpression {
     std::string name;
@@ -31,7 +48,7 @@ BOOST_FUSION_ADAPT_STRUCT(VariableExpression, name);
 
 
 struct IndexExpression {
-    VariableExpression name;
+    std::string name;
     std::vector<int> args;
 };
 
@@ -48,14 +65,14 @@ struct UnaryExpression {
 BOOST_FUSION_ADAPT_STRUCT(UnaryExpression, op, arg)
 
 
-struct BinaryExpression {
-    enum op_t { Plus, Minus, Mul, Div, Mod, Pow, };
+struct EqualityExpression {
+    enum op_t { Equality };
 
-    Expression first;
-    std::vector<std::pair<op_t, Expression>> ops;
+    IndexExpression first;
+    Expression last;
 };
 
-BOOST_FUSION_ADAPT_STRUCT(BinaryExpression, first, ops)
+BOOST_FUSION_ADAPT_STRUCT(EqualityExpression, first, last)
 
 
 struct FunctionCall {
@@ -63,7 +80,12 @@ struct FunctionCall {
     std::vector<Expression> args;
 };
 
+
 BOOST_FUSION_ADAPT_STRUCT(FunctionCall, function, args)
+
+struct equality_op_t : x3::symbols<EqualityExpression::op_t> {
+     equality_op_t();
+} const equality_op;
 
 
 struct unary_op_t : x3::symbols<UnaryExpression::op_t> {
@@ -71,10 +93,22 @@ struct unary_op_t : x3::symbols<UnaryExpression::op_t> {
 } const unary_op;
 
 
+struct BinaryExpression {
+    enum op_t { Plus, Minus, Mul, Div, Mod, Pow, Equality};
+
+    Expression first;
+    std::vector<std::pair<op_t, Expression>> ops;
+};
+
+
+BOOST_FUSION_ADAPT_STRUCT(BinaryExpression, first, ops)
+
+
 struct binary_op : x3::symbols<BinaryExpression::op_t> {
     explicit binary_op(int precedence);
 };
 
+x3::rule<class index_variable, IndexExpression> const index_variable;
 x3::rule<class variable, VariableExpression> const variable;
 x3::rule<class identifier, std::string> const identifier;
 x3::rule<class simple_expr, Expression> const simple_expr;
@@ -83,6 +117,7 @@ x3::rule<class function_call, FunctionCall> const function_call;
 x3::rule<class binary_expr_1, BinaryExpression> const binary_expr_1;
 x3::rule<class binary_expr_2, BinaryExpression> const binary_expr_2;
 x3::rule<class binary_expr_3, BinaryExpression> const binary_expr_3;
+x3::rule<class equality, EqualityExpression> const equality;
 x3::rule<class expr, Expression> const expr;
 
 auto const identifier_def = x3::raw[+(x3::alpha | '_')];
@@ -92,19 +127,24 @@ auto const simple_expr_def
     | '(' >> expr >> ')'
     | unary_expr
     | function_call
+    | index_variable
     | variable
     ;
 
 auto const unary_expr_def = unary_op >> simple_expr;
 
-auto const function_call_def = identifier >> '(' >> (expr % ',') >> ')';
+auto const equality_def = index_variable >> '=' >> simple_expr;
+
+auto const function_call_def = identifier >> '(' >>  (expr % ',') >> ')';
 auto const variable_def = x3::raw[+(x3::alpha)];
-auto const variable_index_def = variable >> '[' >> (x3::int_ % ':') >> ']';
+auto const index_variable_def = x3::raw[+(x3::alpha)] >> '[' >> (x3::int_ % ':') >> ']';
+
 
 auto const binary_expr_1_def = binary_expr_2 >> *(binary_op(1) >> binary_expr_2);
 auto const binary_expr_2_def = binary_expr_3 >> *(binary_op(2) >> binary_expr_3);
 auto const binary_expr_3_def = simple_expr >> *(binary_op(3) >> simple_expr);
 
+//auto const expr_def = equality;
 auto const expr_def = binary_expr_1;
 
 
@@ -113,16 +153,30 @@ BOOST_SPIRIT_DEFINE(
     simple_expr,
     unary_expr,
     function_call,
-    binary_expr_1, binary_expr_2, binary_expr_3,
-    expr, variable);
+    equality,
+    binary_expr_1, binary_expr_2,  binary_expr_3,
+    expr, variable, index_variable);
 
 class FormulaParser {
+    
+private:
+    std::map<std::string, double> constants;
+    std::string path = "./";
+    bool presence_of_IndexExpression = false; 
+    std::vector <double> data_;
+    int maxindex = 0;
+    std::vector<double> eval_binary(BinaryExpression::op_t op, std::vector<double> a, std::vector<double> b);
+    //double eval_binary(BinaryExpression::op_t op, double a, double b);
+    std::vector<double> eval(Expression e);
+    double return_variable(std::string name);
+    void load_constants(std::string path);
+    //Expression preParse(const char* input);
+    void FormulaParser::Equality(Expression first, Expression second);
+
 public:
-    FormulaParser() {};
-
-    Expression parse(const char* input);
+    FormulaParser() { load_constants(path);};
     ~FormulaParser() {};
-    double eval_binary(BinaryExpression::op_t op, double a, double b);
-    double eval(Expression e);
-
+    void parse(std::string input_t);
+    //std::vector <double> parse(std::string input);
 };
+
